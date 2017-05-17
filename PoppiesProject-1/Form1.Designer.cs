2a933df8a454add17f2981d0aecdfbc099341273
Samuel <Samuel@SPATTERSON-LAP>
2017-05-17 12:41:55 +0100

    Connected to the database and added a query to show a table such as customers or employees.

--- PoppiesProject-1/Form1.Designer.cs
+++ PoppiesProject-1/Form1.Designer.cs
@@ -28,21 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.databaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.results = new System.Windows.Forms.DataGridView();
+            this.query_box = new System.Windows.Forms.TextBox();
+            this.queryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runQueryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.results)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.databaseToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.queryToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(578, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // databaseToolStripMenuItem
+            // 
+            this.databaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem,
+            this.disconnectToolStripMenuItem});
+            this.databaseToolStripMenuItem.Name = "databaseToolStripMenuItem";
+            this.databaseToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.databaseToolStripMenuItem.Text = "Database";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.connectToolStripMenuItem.Text = "Connect";
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.connectToolStripMenuItem_Click);
+            // 
+            // disconnectToolStripMenuItem
+            // 
+            this.disconnectToolStripMenuItem.Name = "disconnectToolStripMenuItem";
+            this.disconnectToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.disconnectToolStripMenuItem.Text = "Disconnect";
+            this.disconnectToolStripMenuItem.Click += new System.EventHandler(this.disconnectToolStripMenuItem_Click);
+            // 
+            // results
+            // 
+            this.results.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.results.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.results.Location = new System.Drawing.Point(0, 24);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(578, 257);
+            this.results.TabIndex = 1;
+            this.results.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.results_CellContentClick);
+            // 
+            // query_box
+            // 
+            this.query_box.Dock = System.Windows.Forms.DockStyle.Top;
+            this.query_box.Location = new System.Drawing.Point(0, 24);
+            this.query_box.Multiline = true;
+            this.query_box.Name = "query_box";
+            this.query_box.Size = new System.Drawing.Size(578, 113);
+            this.query_box.TabIndex = 2;
+            // 
+            // queryToolStripMenuItem
+            // 
+            this.queryToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runQueryToolStripMenuItem});
+            this.queryToolStripMenuItem.Name = "queryToolStripMenuItem";
+            this.queryToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.queryToolStripMenuItem.Text = "Query";
+            // 
+            // runQueryToolStripMenuItem
+            // 
+            this.runQueryToolStripMenuItem.Name = "runQueryToolStripMenuItem";
+            this.runQueryToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.runQueryToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.runQueryToolStripMenuItem.Text = "Run Query ";
+            this.runQueryToolStripMenuItem.Click += new System.EventHandler(this.runQueryToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(578, 281);
+            this.Controls.Add(this.query_box);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.results)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem databaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.DataGridView results;
+        private System.Windows.Forms.ToolStripMenuItem queryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runQueryToolStripMenuItem;
+        private System.Windows.Forms.TextBox query_box;
     }
 }
 
--- PoppiesProject-1/Form1.cs
+++ PoppiesProject-1/Form1.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace PoppiesProject_1
 {
     public partial class Form1 : Form
     {
+        string conn_string = "Provider = Microsoft.ACE.OLEDB.12.0;" +
+                @"Data Source= C:\Users\Samuel\Desktop\Poppies Database_090517.accdb";
+        string error_msg = "You are not connected to the Database";
+        string q = "";
+        OleDbConnection conn = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,43 +26,85 @@ namespace PoppiesProject_1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            connectToolStripMenuItem.PerformClick();
         }
 
-        public void ConnectToAccess()
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            System.Data.OleDb.OleDbConnection conn = new
-                System.Data.OleDb.OleDbConnection();
-            //TODO: Modify the connection string and include any
-            //additional required properties for your database.
-            conn.ConnectionString = @"Provider = Microsoft.ACE.OLEDB.12.0;" +
-                @"Data Source= C:\Users\Samuel\Desktop\Poppies Database_090517.accdb" +
-                @"Poppies Database_090517.accdb";
-
             try
             {
+                conn = new OleDbConnection(conn_string);
                 conn.Open();
-                MessageBox.Show("Connected!!");
+                disconnectToolStripMenuItem.Enabled = true;
+                connectToolStripMenuItem.Enabled = false;
+                MessageBox.Show("Database connected");
+
+
+            }
+
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void disconnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Close();
+                disconnectToolStripMenuItem.Enabled = false;
+                connectToolStripMenuItem.Enabled = true;
+            }
+
+            catch (Exception ex) { MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void run_Query()
+        {
+            error_msg = "";
+            q = query_box.Text;
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand(q, conn);
+                OleDbDataAdapter a = new OleDbDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+
+                a.SelectCommand = cmd;
+                a.Fill(dt);
+
+                results.DataSource = dt;
+                results.AutoResizeColumns();
+
+                
+                
             }
 
             catch (Exception ex)
-
             {
+                error_msg = ex.Message;
+                MessageBox.Show(error_msg);
 
-                MessageBox.Show("Failed to connect to data source");
 
             }
+        }
 
-            finally
-            {
-                conn.Close();
-            }
+        private void results_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
         }
 
-
+        private void runQueryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            run_Query();
+            this.Cursor = Cursors.Default; 
+        }
     }
-
-
 }
+       
 
